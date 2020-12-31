e2443d824ae0d0707852b96c8ec7135bbaa54baf
Pankaj Swami <pankaj@elemensissoftech.com>
2020-12-31 10:22:37 +0530

    changes in database connection

--- app/database.js
+++ app/database.js
@@ -2,10 +2,10 @@ const util = require('util');
 let mysql = require('mysql');
 
 global.connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Binali@123',
-    database: 'Employees',
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bf26387d579e0a',
+    password: '3786e338',
+    database: 'heroku_0504f9d559a766f',
     charset: 'utf8mb4'
 });
 connection.connect();
