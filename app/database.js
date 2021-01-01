d86722beef69712e5f8d9113e1f87eaf6251c667
Pankaj Swami <pankaj@elemensissoftech.com>
2021-01-01 11:54:49 +0530

    changes in login API

--- app/database.js
+++ app/database.js
@@ -2,10 +2,10 @@ const util = require('util');
 let mysql = require('mysql');
 
 global.connection = mysql.createConnection({
-    host: 'us-cdbr-east-02.cleardb.com',
-    user: 'bf26387d579e0a',
-    password: '3786e338',
-    database: 'heroku_0504f9d559a766f',
+    host: 'localhost',
+    user: 'root',
+    password: 'Binali@123',
+    database: 'Employees',
     charset: 'utf8mb4'
 });
 connection.connect();
