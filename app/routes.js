b28b9b88c14ad6041fef65ae23aa7ab31e4da88b
Binali <binali@elemensissoftech.com>
2021-02-02 10:45:41 +0530

    changes in routes

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,15 @@
 const basic = require('./controllers/basic');
-const user = require('./controllers/user');
+const emp = require('./controllers/emp');
 const common = require('./util/common');
 
 module.exports = function(app) {     
 
     app.post("/login", basic.login);// 1
     app.post("/registration", basic.registration);// 2
-    app.get("/getEmployeeList", common.verifyToken, user.getEmployeeList);// 3
-    app.get("/getEmployeeDetails/:userName", common.verifyToken, user.getEmployeeDetails);// 4
-    app.put("/updateEmployee", common.verifyToken, user.updateEmployee);// 5
-    app.post("/addEmployee", common.verifyToken, user.addEmployee);// 6
-    app.delete("/deleteEmployee/:id", user.deleteEmployee);// 7
+    app.get("/getEmployeeList", common.verifyToken, emp.getEmployeeList);// 3
+    app.get("/getEmployeeDetails/:userName", common.verifyToken, emp.getEmployeeDetails);// 4
+    app.put("/updateEmployee", common.verifyToken, emp.updateEmployee);// 5
+    app.post("/addEmployee", common.verifyToken, emp.addEmployee);// 6
+    app.delete("/deleteEmployee/:id", emp.deleteEmployee);// 7
     app.get("/verifyToken",common.verifyToken);//8
 }
