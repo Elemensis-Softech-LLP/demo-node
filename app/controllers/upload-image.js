53ccda71ba985ed205451a62fb22d7d475664ee2
Binali <binali@elemensissoftech.com>
2021-02-04 18:13:52 +0530

    change in image api

--- app/controllers/upload-image.js
+++ app/controllers/upload-image.js
@@ -1,31 +1,48 @@
-
-const express = require('express');
-const app = express();
-const path = require('path');
-const multer = require('multer');
-
-
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'pictures/jpeg' || file.mimetype == 'pictures/png') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
-const upload = multer({ storage: storage, fileFilter: fileFilter });
-
 module.exports = {
     upload
+}
+
+async function upload(req, res) {
+    try {
+        var multer = require('multer');   //FOR FILE UPLOAD
+        var storage = multer.diskStorage({ //multers disk storage settings
+            destination: function (req, file, cb) {
+                cb(null, './app/public/'); //image storage path
+            },
+            filename: function (req, file, cb) {
+                var datetimestamp = Date.now();
+                cb(null, file.originalname);
+            }
+            
+        });
+
+        var upload = multer({ //multer settings
+            storage: storage
+        }).single('image');
+          
+        upload(req, res, function (err) {
+            // console.log("my error============", err)
+            const path = req.file.path;
+            if (err) {
+                res.send({
+                    'success': false,
+                    'message': 'an Error occured'
+                });
+            } else {
+                res.send({
+                    'success': true,
+                    path: path,
+                    'message': 'image upload successfully'
+                });
+            }
+
+        });
+    } catch (error) {
+
+        res.status(500).send({
+            'success': false,
+            'message': 'Error! invalid',
+            'error': error
+        });
+    }
 }
--- app/server.js
+++ app/server.js
@@ -6,32 +6,53 @@ app.use(cors());
 // app.set('view engine', 'ejs');
 // app.set('views', __dirname + '/../public/views');
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
  
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin','localhost');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept,*');
+//   next();
+// });
+// app.options('*', cors()) // include before other routes\
+require('./routes')(app);
+
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.setHeader('Access-Control-Allow-Origin','http://localhost:4201');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
-var enableCORS = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept,*');
 
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
-  };
 
-  req.on('information', (info) => {
-     });
-};
-app.use(enableCORS);
+// app.use(cors());
+// app.options('http://localhost:4200', cors());  // enable pre-flight
+
+// var enableCORS = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept,*');
+
+//   if ('OPTIONS' == req.method) {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   };
+
+//   req.on('information', (info) => {
+//      });
+// };
+// app.use(enableCORS);
 // parse application/json
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/../public'));
-require('./routes')(app);
+// app.use('public',express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
 require('./database');
 module.exports = app;
